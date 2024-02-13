0ab8ce6c5b68da21efb654ddf465f24bf9497baa
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-13 09:51:57 +0300

    Initial Commit

---
+++ SwiftUI Charts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Charts
+//
+//  Created by Ahmet Ozen on 13.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Charts/SwiftUI_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChartsApp.swift
+//  SwiftUI Charts
+//
+//  Created by Ahmet Ozen on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
