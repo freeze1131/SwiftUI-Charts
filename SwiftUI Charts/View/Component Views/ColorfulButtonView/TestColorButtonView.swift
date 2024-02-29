d4803785ef3e84c65b4d423c78a26279450b3f2e
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-29 15:40:46 +0300

    added basic chart view

--- SwiftUI Charts/View/Component Views/ColorfulButtonView/TestColorButtonView.swift
+++ SwiftUI Charts/View/Component Views/ColorfulButtonView/TestColorButtonView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct TestColorButtonVie: View {
+    @State var colors: [Color]
+    var count: CGFloat {
+        CGFloat(colors.count)
+    }
+    var barWidth: CGFloat {
+        (UIScreen.main.bounds.width / count) - 5
+    }
+    @State private var rotateBar: Bool = false
+    @State private var tiltBar: Double = 0.0
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            HStack(alignment: .bottom,spacing: 5) {
+               
+                ForEach(colors,id:\.self) { color in
+                    let height = Double.random(in: 50...200)
+                    BorderedRectangle(color: color, barWidth: barWidth, height: height)
+                }
+            }
+        }
+    }
+}
+
+
+
+struct BorderedRectangle: View {
+    let color: Color
+    let barWidth: Double
+    let height: Double
+    var body: some View {
+        RoundedRectangle(cornerRadius: 5)
+            .fill(color)
+            .frame(width: barWidth,height: height)
+            .overlay(
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(lineWidth: 0.7))
     }
 }
 
 #Preview {
-    TestColorButtonVie()
+    TestColorButtonVie(colors: Color.defaultColors)
 }
