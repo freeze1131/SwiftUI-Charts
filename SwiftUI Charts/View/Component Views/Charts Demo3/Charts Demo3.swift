6706b8db5abbadb8850e3cb4d21051a65464b442
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-29 19:40:39 +0300

    added more chart types and made transition between them

--- SwiftUI Charts/View/Component Views/Charts Demo3/Charts Demo3.swift
+++ SwiftUI Charts/View/Component Views/Charts Demo3/Charts Demo3.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 import Charts
 
+enum ChartType {
+    case bar,line,area
+}
+
 
 struct Charts_Demo3: View {
     let dailySales: [DailySalesType]
     let min: Double
     let max: Double
     let barColors: [Color]
+    @State var  chartType: ChartType = .bar
     let xAxisMarkPosition: AxisMarkPosition = .bottom
     let yAxisMarkPosition: AxisMarkPosition = .leading
     @State private var isVerticalChart = true
@@ -22,15 +27,48 @@ struct Charts_Demo3: View {
     var body: some View {
         NavigationStack {
             VStack {
-                
                 Chart {
                     ForEach(dailySales) { item in
-                        BarMark(x: .value("Day", item.day), y: .value("Sales", item.sales))
-                            .foregroundStyle(by: .value("Day", item.day))
+                        switch(chartType) {
+                        case  .bar:
+                            BarMark( x: .value("Sales", item.sales),y: .value("Day", item.day))
+                                .foregroundStyle(by: .value("Day", item.day))
+                        case  .line:
+                            LineMark( x: .value("Sales", item.sales),y: .value("Day", item.day))
+                        case  .area:
+                            AreaMark( x: .value("Sales", item.sales),y: .value("Day", item.day))
+                        }
+                      
                     }
                 }
+                .chartLegend(.hidden)
                 
-                
+                HStack(spacing: 30) {
+                    Button(action: {
+                        withAnimation {
+                            chartType = .bar
+                        }
+                    }, label: {
+                        Text("BAR")
+                    })
+                    
+                    Button(action: {
+                        withAnimation {
+                            chartType = .line
+                        }
+                    }, label: {
+                        Text("LINE")
+                    })
+                    
+                    Button(action: {
+                        withAnimation {
+                            chartType = .area
+                        }
+                    }, label: {
+                        Text("AREA")
+                    })
+                }
+  
             }
             .padding()
             .navigationTitle("Demo 3")
