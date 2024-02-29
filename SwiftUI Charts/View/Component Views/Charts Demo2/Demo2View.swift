117a5ab66881261685b5f4a87b8f0f29da9fa442
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-29 18:53:54 +0300

    added annotaions to chart

--- SwiftUI Charts/View/Component Views/Charts Demo2/Demo2View.swift
+++ SwiftUI Charts/View/Component Views/Charts Demo2/Demo2View.swift
@@ -13,44 +13,49 @@ struct Demo2View: View {
     let max = 1000.0
     
     var body: some View {
-        Text("Hello, World!")
-            .font(.largeTitle)
-        
-        Chart {
-            BarMark(x: .value("Day", "Sun"),y: .value("Sales", 100))
-                .annotation {
-                    Image(systemName: "figure.walk.circle.fill")
-                        .foregroundStyle(Color.blueViolet)
-                        .fontWeight(.bold)
-                        .font(.title)
-                }
+       
+        VStack(alignment:.center) {
+            Text("Hello, World!")
+                .font(.largeTitle)
             
-            BarMark(x: .value("Day", "Mon"),y: .value("Sales", 250))
-                .annotation(position: .top, alignment: .center) {
-                    Image(systemName: "figure.dance")
-                        .foregroundStyle(Color.orangeRed)
-                        .fontWeight(.bold)
-                        .font(.title)
-                }
-            
-            BarMark(x: .value("Day", "Tue"),y: .value("Sales", 75.3))
-            
-            BarMark(x: .value("Day", "Wed"),y: .value("Sales", 850))
-            
-            BarMark(x: .value("Day", "Thu"),y: .value("Sales", 450))
-            
-            BarMark(x: .value("Day", "Fri"),y: .value("Sales", 20.23))
-            
-            BarMark(x: .value("Day", "Sat"),y: .value("Sales", 300))
-
+            Chart {
+                BarMark(x: .value("Day", "Sun"),y: .value("Sales", 100))
+                    .annotation {
+                        Image(systemName: "figure.walk.circle.fill")
+                            .foregroundStyle(Color.blueViolet)
+                            .fontWeight(.bold)
+                            .font(.title)
+                    }
+                    .foregroundStyle(by: .value("Day", "Sun"))
+                
+                BarMark(x: .value("Day", "Mon"),y: .value("Sales", 250))
+                    .annotation(position: .top, alignment: .center) {
+                        Image(systemName: "figure.dance")
+                            .foregroundStyle(Color.orangeRed)
+                            .fontWeight(.bold)
+                            .font(.title)
+                    }
+                    .foregroundStyle(by: .value("Day", "Mon"))
+                
+                BarMark(x: .value("Day", "Tue"),y: .value("Sales", 75.3))
+                    .foregroundStyle(by: .value("Day", "Tue"))
+                
+                BarMark(x: .value("Day", "Wed"),y: .value("Sales", 850))
+                    .foregroundStyle(by: .value("Day", "Wed"))
+                
+                BarMark(x: .value("Day", "Thu"),y: .value("Sales", 450))
+                    .foregroundStyle(by: .value("Day", "Thu"))
+                BarMark(x: .value("Day", "Fri"),y: .value("Sales", 20.23))
+                    .foregroundStyle(by: .value("Day", "Fri"))
+                BarMark(x: .value("Day", "Sat"),y: .value("Sales", 300))
+                    .foregroundStyle(by: .value("Day", "Sat"))
+            }
+            .chartYScale(domain: min...max)
+            .chartYAxis {
+                AxisMarks (position: .trailing)
+            }
         }
-        .chartYScale(domain: min...max)
-        .chartXAxis {
-            AxisMarks(position:.bottom)
-        }
-        .chartYAxis {
-            AxisMarks (position: .trailing)
-        }
+        .padding()
     }
 }
 
