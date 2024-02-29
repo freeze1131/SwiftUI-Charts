5b5cf1ee1fc007b849d8a51fca30e61cb7201596
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-29 14:46:22 +0300

    added colors, now generating random arcview according to color

--- SwiftUI Charts/Extensions/ColorExtension.swift
+++ SwiftUI Charts/Extensions/ColorExtension.swift
@@ -1,8 +1,325 @@
-//
-//  ColorExtension.swift
-//  SwiftUI Charts
-//
-//  Created by Ahmet Ozen on 13.02.2024.
-//
 
-import Foundation
+import SwiftUI
+
+// Based on
+// https://www.rapidtables.com/web/color/RGB_Color.html
+
+extension Color {
+    static let maroon = Color( red: 128 / 255.0, green: 0 / 255.0, blue: 0)
+    static let darkRed = Color( red: 139 / 255.0, green: 0 / 255.0, blue: 0)
+    static let brown1 = Color( red: 165 / 255.0, green: 42 / 255.0, blue: 42)
+    static let firebrick = Color( red: 178 / 255.0, green: 34 / 255.0, blue: 34)
+    static let crimson = Color( red: 220 / 255.0, green: 20 / 255.0, blue: 60)
+    static let red1 = Color( red: 255 / 255.0, green: 0 / 255.0, blue: 0)
+    static let tomato = Color( red: 255 / 255.0, green: 99 / 255.0, blue: 71)
+    static let coral = Color( red: 255 / 255.0, green: 127 / 255.0, blue: 80)
+    static let indianRed = Color( red: 205 / 255.0, green: 92 / 255.0, blue: 92)
+    static let lightCoral = Color( red: 240 / 255.0, green: 128 / 255.0, blue: 128)
+    static let darkSalmon = Color( red: 233 / 255.0, green: 150 / 255.0, blue: 122)
+    static let salmon = Color( red: 250 / 255.0, green: 128 / 255.0, blue: 114)
+    static let lightSalmon = Color( red: 255 / 255.0, green: 160 / 255.0, blue: 122)
+    static let orangeRed = Color( red: 255 / 255.0, green: 69 / 255.0, blue: 0)
+    static let darkOrange = Color( red: 255 / 255.0, green: 140 / 255.0, blue: 0)
+    static let orange1 = Color( red: 255 / 255.0, green: 165 / 255.0, blue: 0)
+    static let gold = Color( red: 255 / 255.0, green: 215 / 255.0, blue: 0)
+    static let darkGoldenRod = Color( red: 184 / 255.0, green: 134 / 255.0, blue: 11)
+    static let goldenRod = Color( red: 218 / 255.0, green: 165 / 255.0, blue: 32)
+    static let paleGoldenRod = Color( red: 238 / 255.0, green: 232 / 255.0, blue: 170)
+    static let darkKhaki = Color( red: 189 / 255.0, green: 183 / 255.0, blue: 107)
+    static let khaki = Color( red: 240 / 255.0, green: 230 / 255.0, blue: 140)
+    static let olive = Color( red: 128 / 255.0, green: 128 / 255.0, blue: 0)
+    static let yellow1 = Color( red: 255 / 255.0, green: 255 / 255.0, blue: 0)
+    static let yellowGreen = Color( red: 154 / 255.0, green: 205 / 255.0, blue: 50)
+    static let darkOliveGreen = Color( red: 85 / 255.0, green: 107 / 255.0, blue: 47)
+    static let oliveDrab = Color( red: 107 / 255.0, green: 142 / 255.0, blue: 35)
+    static let lawnGreen = Color( red: 124 / 255.0, green: 252 / 255.0, blue: 0)
+    static let chartreuse = Color( red: 127 / 255.0, green: 255 / 255.0, blue: 0)
+    static let greenYellow = Color( red: 173 / 255.0, green: 255 / 255.0, blue: 47)
+    static let darkGreen = Color( red: 0 / 255.0, green: 100 / 255.0, blue: 0)
+    static let green1 = Color( red: 0 / 255.0, green: 128 / 255.0, blue: 0)
+    static let forestGreen = Color( red: 34 / 255.0, green: 139 / 255.0, blue: 34)
+    static let lime = Color( red: 0 / 255.0, green: 255 / 255.0, blue: 0)
+    static let limeGreen = Color( red: 50 / 255.0, green: 205 / 255.0, blue: 50)
+    static let lightGreen = Color( red: 144 / 255.0, green: 238 / 255.0, blue: 144)
+    static let paleGreen = Color( red: 152 / 255.0, green: 251 / 255.0, blue: 152)
+    static let darkSeaGreen = Color( red: 143 / 255.0, green: 188 / 255.0, blue: 143)
+    static let mediumSpringGreen = Color( red: 0 / 255.0, green: 250 / 255.0, blue: 154)
+    static let springGreen = Color( red: 0 / 255.0, green: 255 / 255.0, blue: 127)
+    static let seaGreen = Color( red: 46 / 255.0, green: 139 / 255.0, blue: 87)
+    static let mediumAquaMarine = Color( red: 102 / 255.0, green: 205 / 255.0, blue: 170)
+    static let mediumSeaGreen = Color( red: 60 / 255.0, green: 179 / 255.0, blue: 113)
+    static let lightSeaGreen = Color( red: 32 / 255.0, green: 178 / 255.0, blue: 170)
+    static let darkSlateGray = Color( red: 47 / 255.0, green: 79 / 255.0, blue: 79)
+    static let teal1 = Color( red: 0 / 255.0, green: 128 / 255.0, blue: 128)
+    static let darkCyan = Color( red: 0 / 255.0, green: 139 / 255.0, blue: 139)
+    static let aqua = Color( red: 0 / 255.0, green: 255 / 255.0, blue: 255)
+    static let cyan1 = Color( red: 0 / 255.0, green: 255 / 255.0, blue: 255)
+    static let lightCyan = Color( red: 224 / 255.0, green: 255 / 255.0, blue: 255)
+    static let darkTurquoise = Color( red: 0 / 255.0, green: 206 / 255.0, blue: 209)
+    static let turquoise = Color( red: 64 / 255.0, green: 224 / 255.0, blue: 208)
+    static let mediumTurquoise = Color( red: 72 / 255.0, green: 209 / 255.0, blue: 204)
+    static let paleTurquoise = Color( red: 175 / 255.0, green: 238 / 255.0, blue: 238)
+    static let aquaMarine = Color( red: 127 / 255.0, green: 255 / 255.0, blue: 212)
+    static let powderBlue = Color( red: 176 / 255.0, green: 224 / 255.0, blue: 230)
+    static let cadetBlue = Color( red: 95 / 255.0, green: 158 / 255.0, blue: 160)
+    static let steelBlue = Color( red: 70 / 255.0, green: 130 / 255.0, blue: 180)
+    static let cornFlowerBlue = Color( red: 100 / 255.0, green: 149 / 255.0, blue: 237)
+    static let deepSkyBlue = Color( red: 0 / 255.0, green: 191 / 255.0, blue: 255)
+    static let dodgerBlue = Color( red: 30 / 255.0, green: 144 / 255.0, blue: 255)
+    static let lightBlue = Color( red: 173 / 255.0, green: 216 / 255.0, blue: 230)
+    static let skyBlue = Color( red: 135 / 255.0, green: 206 / 255.0, blue: 235)
+    static let lightSkyBlue = Color( red: 135 / 255.0, green: 206 / 255.0, blue: 250)
+    static let midnightBlue = Color( red: 25 / 255.0, green: 25 / 255.0, blue: 112)
+    static let navy = Color( red: 0 / 255.0, green: 0 / 255.0, blue: 128)
+    static let darkBlue = Color( red: 0 / 255.0, green: 0 / 255.0, blue: 139)
+    static let mediumBlue = Color( red: 0 / 255.0, green: 0 / 255.0, blue: 205)
+    static let blue1 = Color( red: 0 / 255.0, green: 0 / 255.0, blue: 255)
+    static let royalBlue = Color( red: 65 / 255.0, green: 105 / 255.0, blue: 225)
+    static let blueViolet = Color( red: 138 / 255.0, green: 43 / 255.0, blue: 226)
+    static let indigo1 = Color( red: 75 / 255.0, green: 0 / 255.0, blue: 130)
+    static let darkSlateBlue = Color( red: 72 / 255.0, green: 61 / 255.0, blue: 139)
+    static let slateBlue = Color( red: 106 / 255.0, green: 90 / 255.0, blue: 205)
+    static let mediumSlateBlue = Color( red: 123 / 255.0, green: 104 / 255.0, blue: 238)
+    static let mediumPurple = Color( red: 147 / 255.0, green: 112 / 255.0, blue: 219)
+    static let darkMagenta = Color( red: 139 / 255.0, green: 0 / 255.0, blue: 139)
+    static let darkViolet = Color( red: 148 / 255.0, green: 0 / 255.0, blue: 211)
+    static let darkOrchid = Color( red: 153 / 255.0, green: 50 / 255.0, blue: 204)
+    static let mediumOrchid = Color( red: 186 / 255.0, green: 85 / 255.0, blue: 211)
+    static let purple1 = Color( red: 128 / 255.0, green: 0 / 255.0, blue: 128)
+    static let thistle = Color( red: 216 / 255.0, green: 191 / 255.0, blue: 216)
+    static let plum = Color( red: 221 / 255.0, green: 160 / 255.0, blue: 221)
+    static let violet = Color( red: 238 / 255.0, green: 130 / 255.0, blue: 238)
+    static let magenta = Color( red: 255 / 255.0, green: 0 / 255.0, blue: 255)
+    static let orchid = Color( red: 218 / 255.0, green: 112 / 255.0, blue: 214)
+    static let mediumVioletRed = Color( red: 199 / 255.0, green: 21 / 255.0, blue: 133)
+    static let paleVioletRed = Color( red: 219 / 255.0, green: 112 / 255.0, blue: 147)
+    static let deepPink = Color( red: 255 / 255.0, green: 20 / 255.0, blue: 147)
+    static let hotPink = Color( red: 255 / 255.0, green: 105 / 255.0, blue: 180)
+    static let lightPink = Color( red: 255 / 255.0, green: 182 / 255.0, blue: 193)
+    static let pink1 = Color( red: 255 / 255.0, green: 192 / 255.0, blue: 203)
+    static let antiqueWhite = Color( red: 250 / 255.0, green: 235 / 255.0, blue: 215)
+    static let beige = Color( red: 245 / 255.0, green: 245 / 255.0, blue: 220)
+    static let bisque = Color( red: 255 / 255.0, green: 228 / 255.0, blue: 196)
+    static let blanchedAlmond = Color( red: 255 / 255.0, green: 235 / 255.0, blue: 205)
+    static let wheat = Color( red: 245 / 255.0, green: 222 / 255.0, blue: 179)
+    static let cornSilk = Color( red: 255 / 255.0, green: 248 / 255.0, blue: 220)
+    static let lemonChiffon = Color( red: 255 / 255.0, green: 250 / 255.0, blue: 205)
+    static let lightGoldenRodYellow = Color( red: 250 / 255.0, green: 250 / 255.0, blue: 210)
+    static let lightYellow = Color( red: 255 / 255.0, green: 255 / 255.0, blue: 224)
+    static let saddleBrown = Color( red: 139 / 255.0, green: 69 / 255.0, blue: 19)
+    static let sienna = Color( red: 160 / 255.0, green: 82 / 255.0, blue: 45)
+    static let chocolate = Color( red: 210 / 255.0, green: 105 / 255.0, blue: 30)
+    static let peru = Color( red: 205 / 255.0, green: 133 / 255.0, blue: 63)
+    static let sandyBrown = Color( red: 244 / 255.0, green: 164 / 255.0, blue: 96)
+    static let burlyWood = Color( red: 222 / 255.0, green: 184 / 255.0, blue: 135)
+    static let tan = Color( red: 210 / 255.0, green: 180 / 255.0, blue: 140)
+    static let rosyBrown = Color( red: 188 / 255.0, green: 143 / 255.0, blue: 143)
+    static let moccasin = Color( red: 255 / 255.0, green: 228 / 255.0, blue: 181)
+    static let navajoWhite = Color( red: 255 / 255.0, green: 222 / 255.0, blue: 173)
+    static let peachPuff = Color( red: 255 / 255.0, green: 218 / 255.0, blue: 185)
+    static let mistyRose = Color( red: 255 / 255.0, green: 228 / 255.0, blue: 225)
+    static let lavenderBlush = Color( red: 255 / 255.0, green: 240 / 255.0, blue: 245)
+    static let linen = Color( red: 250 / 255.0, green: 240 / 255.0, blue: 230)
+    static let oldLace = Color( red: 253 / 255.0, green: 245 / 255.0, blue: 230)
+    static let papayaWhip = Color( red: 255 / 255.0, green: 239 / 255.0, blue: 213)
+    static let seaShell = Color( red: 255 / 255.0, green: 245 / 255.0, blue: 238)
+    static let mintCream = Color( red: 245 / 255.0, green: 255 / 255.0, blue: 250)
+    static let slateGray = Color( red: 112 / 255.0, green: 128 / 255.0, blue: 144)
+    static let lightSlateGray = Color( red: 119 / 255.0, green: 136 / 255.0, blue: 153)
+    static let lightSteelBlue = Color( red: 176 / 255.0, green: 196 / 255.0, blue: 222)
+    static let lavender = Color( red: 230 / 255.0, green: 230 / 255.0, blue: 250)
+    static let floralWhite = Color( red: 255 / 255.0, green: 250 / 255.0, blue: 240)
+    static let aliceBlue = Color( red: 240 / 255.0, green: 248 / 255.0, blue: 255)
+    static let ghostWhite = Color( red: 248 / 255.0, green: 248 / 255.0, blue: 255)
+    static let honeydew = Color( red: 240 / 255.0, green: 255 / 255.0, blue: 240)
+    static let ivory = Color( red: 255 / 255.0, green: 255 / 255.0, blue: 240)
+    static let azure = Color( red: 240 / 255.0, green: 255 / 255.0, blue: 255)
+    static let snow = Color( red: 255 / 255.0, green: 250 / 255.0, blue: 250)
+    static let black1 = Color( red: 0 / 255.0, green: 0 / 255.0, blue: 0)
+    static let dimGray = Color( red: 105 / 255.0, green: 105 / 255.0, blue: 105)
+    static let gray1 = Color( red: 128 / 255.0, green: 128 / 255.0, blue: 128)
+    static let darkGray = Color( red: 169 / 255.0, green: 169 / 255.0, blue: 169)
+    static let silver = Color( red: 192 / 255.0, green: 192 / 255.0, blue: 192)
+    static let lightGray = Color( red: 211 / 255.0, green: 211 / 255.0, blue: 211)
+    static let gainsboro = Color( red: 220 / 255.0, green: 220 / 255.0, blue: 220)
+    static let whiteSmoke = Color( red: 245 / 255.0, green: 245 / 255.0, blue: 245)
+    static let white1 = Color( red: 255 / 255.0, green: 255 / 255.0, blue: 255)
+
+
+}
+
+var mainColorList: [Color] = [
+    .maroon,
+    .darkRed,
+    .brown1,
+    .firebrick,
+    .crimson,
+    .red1,
+    .tomato,
+    .coral,
+    .indianRed,
+    .lightCoral,
+    .darkSalmon,
+    .salmon,
+    .lightSalmon,
+    .orangeRed,
+    .darkOrange,
+    .orange1,
+    .gold,
+    .darkGoldenRod,
+    .goldenRod,
+    .paleGoldenRod,
+    .darkKhaki,
+    .khaki,
+    .olive,
+    .yellow1,
+    .yellowGreen,
+    .darkOliveGreen,
+    .oliveDrab,
+    .lawnGreen,
+    .chartreuse,
+    .greenYellow,
+    .darkGreen,
+    .green1,
+    .forestGreen,
+    .lime,
+    .limeGreen,
+    .lightGreen,
+    .paleGreen,
+    .darkSeaGreen,
+    .mediumSpringGreen,
+    .springGreen,
+    .seaGreen,
+    .mediumAquaMarine,
+    .mediumSeaGreen,
+    .lightSeaGreen,
+    .darkSlateGray,
+    .teal1,
+    .darkCyan,
+    .aqua,
+    .cyan1,
+    .lightCyan,
+    .darkTurquoise,
+    .turquoise,
+    .mediumTurquoise,
+    .paleTurquoise,
+    .aquaMarine,
+    .powderBlue,
+    .cadetBlue,
+    .steelBlue,
+    .cornFlowerBlue,
+    .deepSkyBlue,
+    .dodgerBlue,
+    .lightBlue,
+    .skyBlue,
+    .lightSkyBlue,
+    .midnightBlue,
+    .navy,
+    .darkBlue,
+    .mediumBlue,
+    .blue1,
+    .royalBlue,
+    .blueViolet,
+    .indigo1,
+    .darkSlateBlue,
+    .slateBlue,
+    .mediumSlateBlue,
+    .mediumPurple,
+    .darkMagenta,
+    .darkViolet,
+    .darkOrchid,
+    .mediumOrchid,
+    .purple1,
+    .thistle,
+    .plum,
+    .violet,
+    .magenta,
+    .orchid,
+    .mediumVioletRed,
+    .paleVioletRed,
+    .deepPink,
+    .hotPink,
+    .lightPink,
+    .pink1,
+    .antiqueWhite,
+    .beige,
+    .bisque,
+    .blanchedAlmond,
+    .wheat,
+    .cornSilk,
+    .lemonChiffon,
+    .lightGoldenRodYellow,
+    .lightYellow,
+    .saddleBrown,
+    .sienna,
+    .chocolate,
+    .peru,
+    .sandyBrown,
+    .burlyWood,
+    .tan,
+    .rosyBrown,
+    .moccasin,
+    .navajoWhite,
+    .peachPuff,
+    .mistyRose,
+    .lavenderBlush,
+    .linen,
+    .oldLace,
+    .papayaWhip,
+    .seaShell,
+    .mintCream,
+    .slateGray,
+    .lightSlateGray,
+    .lightSteelBlue,
+    .lavender,
+    .floralWhite,
+    .aliceBlue,
+    .ghostWhite,
+    .honeydew,
+    .ivory,
+    .azure,
+    .snow,
+    .black1,
+    .dimGray,
+    .gray1,
+    .darkGray,
+    .silver,
+    .lightGray,
+    .gainsboro,
+    .whiteSmoke,
+    .white1
+]
+
+extension Color {
+    static var random: Color {
+        return mainColorList
+            .randomElement() ?? .darkRed
+    }
+}
+
+extension Color {
+    static func randomColorsN( n: Int) -> [Color] {
+        var theRandomColors: [Color] = []
+        
+        for _ in 0..<n {
+            theRandomColors
+                .append(Color.random)
+        }
+        
+        return theRandomColors
+    }
+
+}
+
+extension Color {
+    static let defaultColors: [Color] = [
+        .pink,
+         .green,
+         .mint,
+         .purple,
+         .indigo,
+         .red,
+         .brown
+        ]
+}
+
--- SwiftUI Charts/View/Component Views/ColorfulButtonView/RandomArcFromColorListView.swift
+++ SwiftUI Charts/View/Component Views/ColorfulButtonView/RandomArcFromColorListView.swift
@@ -1,18 +1,45 @@
-//
-//  RandomArcFromColorListView.swift
-//  SwiftUI Charts
-//
-//  Created by Ahmet Ozen on 29.02.2024.
-//
+
 
 import SwiftUI
 
 struct RandomArcFromColorListView: View {
+    let colors: [Color]
+    let index: Int
+    let minRadius: CGFloat
+    let maxRadius: CGFloat
+    let opacity: CGFloat
+    
+    
+    // Random constant
+    let startTrim = Double.random(in: 0...0.5)
+    let endTrim = 1.0
+    let rotate = Double.random(in: 0...360)
+
+    // Computed Properties
+    var theIndex: CGFloat {
+        if index < colors.count && index >= 0 {
+            return CGFloat(index)
+        } else {
+            return 0.0
+        }
+    }
+    var count: CGFloat {
+        CGFloat(colors.count)
+    }
+    var color: Color {
+        colors[index]
+    }
+    var endRadius: CGFloat {
+        minRadius + maxRadius / count
+    }
+  
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ArcView(color: color.opacity(opacity), startRadius: minRadius, endRadius: endRadius, startTrim: startTrim, endTrim: endTrim, rotate: rotate)
     }
 }
 
 #Preview {
-    RandomArcFromColorListView()
+    RandomArcFromColorListView(colors: Color.defaultColors, index: 0, minRadius: 170, maxRadius: 180, opacity: 0.9)
 }
