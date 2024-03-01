c8ff778ac561bbfcbf2254b6f2fb7a78291a6d66
freeze1131 <ozenahmetcemil@gmail.com>
2024-03-01 12:18:04 +0300

    added demo 3 to the navgation stack

--- SwiftUI Charts/ContentView.swift
+++ SwiftUI Charts/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
         NavigationStack {
             NavigationLink("Demo 1", destination: TestColorButtonView(colors: Color.defaultColors))
             NavigationLink("Demo 2", destination: Demo2View())
+            NavigationLink("Demo 3", destination:  Charts_Demo3(dailySales: defaultDailySales, min: 0, max: 700, barColors: Color.randomColorsN(n: defaultDailySales.count)))
             .navigationTitle("Charts")
         }
     }
